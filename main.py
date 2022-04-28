83f4d1cc1d142f6bd5146ff951d16b00dccae855
tfeilyu <tfeilyu@outlook.com>
2022-04-28 18:00:39 +0800

    jupyter

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Hello PyCharm!')
 
-    print('你好~ ~')
+    print('你好~23 ~')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
